3a24243fcdda2555d3925cf1dc03acca714dade4
Michael Hausenblas <michael.hausenblas@gmail.com>
2016-12-06 08:30:47 +0000

    inits fintrans influx ingest

--- 1.8/fintrans/README.md
+++ 1.8/fintrans/README.md
@@ -5,6 +5,9 @@
 
 We're using Shopify's [sarama](https://godoc.org/github.com/Shopify/sarama) package to communicate with Kafka.
 
+This repo should be available locally (use: `git clone https://github.com/dcos/demos.git`) we'll call the directory
+it resides in `$DEMO_HOME` in the following.
+
 ### Preparation
 
 [Install](https://github.com/dcos/examples/tree/master/1.8/kafka) the Apache Kafka package with the following [options](kafka-config.json):
@@ -55,7 +58,9 @@ Note that it may be necessary to [add the announced DNS servers]( https://suppor
 With the VPN tunnel enabled, we can run the fintrans generator:
 
 ```bash
-$ ./fintrans --broker broker-0.kafka.mesos:9398
+$ cd $DEMO_HOME/1.8/fintrans/generator/
+$ go build
+$ ./generator --broker broker-0.kafka.mesos:9398
 INFO[0001] &sarama.ProducerMessage{Topic:"London", Key:sarama.Encoder(nil), Value:"678 816 2957", Metadata:interface {}(nil), Offset:10, Partition:0, Timestamp:time.Time{sec:0, nsec:0, loc:(*time.Location)(nil)}, retries:0, flags:0}
 INFO[0003] &sarama.ProducerMessage{Topic:"SF", Key:sarama.Encoder(nil), Value:"762 543 6395", Metadata:interface {}(nil), Offset:4, Partition:0, Timestamp:time.Time{sec:0, nsec:0, loc:(*time.Location)(nil)}, retries:0, flags:0}
 INFO[0005] &sarama.ProducerMessage{Topic:"London", Key:sarama.Encoder(nil), Value:"680 840 8115", Metadata:interface {}(nil), Offset:11, Partition:0, Timestamp:time.Time{sec:0, nsec:0, loc:(*time.Location)(nil)}, retries:0, flags:0}
---
+++ 1.8/fintrans/influx-ingest/main.go
@@ -0,0 +1,90 @@
+// Copyright 2016 Mesosphere. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/Shopify/sarama"
+	log "github.com/Sirupsen/logrus"
+	"os"
+)
+
+const (
+	VERSION string = "0.1.0"
+)
+
+var (
+	version bool
+	// FQDN/IP + port of a Kafka broker:
+	broker string
+	// the Kafka consumer:
+	consumer sarama.Consumer
+)
+
+func about() {
+	fmt.Printf("\nThis is the fintrans InfluxDB ingestion util in version %s\n", VERSION)
+}
+
+func init() {
+	flag.BoolVar(&version, "version", false, "Display version information")
+	flag.StringVar(&broker, "broker", "", "The FQDN or IP address and port of a Kafka broker. Example: broker-1.kafka.mesos:9382 or 10.0.3.178:9398")
+	flag.Usage = func() {
+		fmt.Printf("Usage: %s [args]\n\n", os.Args[0])
+		fmt.Println("Arguments:")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+}
+
+func main() {
+	if version {
+		about()
+		os.Exit(0)
+	}
+	if broker == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if c, err := sarama.NewConsumer([]string{broker}, nil); err != nil {
+		log.Error(err)
+		os.Exit(1)
+	} else {
+		consumer = c
+	}
+	defer func() {
+		if err := consumer.Close(); err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
+	}()
+
+	if partitionConsumer, err := consumer.ConsumePartition("London", 0, sarama.OffsetNewest); err != nil {
+		log.Error(err)
+		os.Exit(1)
+	} else {
+		defer func() {
+			if err := partitionConsumer.Close(); err != nil {
+				log.Fatalln(err)
+			}
+		}()
+
+		for {
+			msg := <-partitionConsumer.Messages()
+			log.Info(fmt.Sprintf("%#v", msg))
+		}
+	}
+}
