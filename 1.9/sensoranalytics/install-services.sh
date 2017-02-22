1b05d7478545624cd273d56a725533d99b01353d
Michael Hausenblas <michael.hausenblas@gmail.com>
2017-02-22 15:39:45 -0800

    fixes 1.9 senseanal services install for Linux
    Kudos to @pleia2 for spotting the bug

--- 1.9/sensoranalytics/install-services.sh
+++ 1.9/sensoranalytics/install-services.sh
@@ -5,12 +5,21 @@ set -o errtrace
 set -o nounset
 set -o pipefail
 
+: ${PUBLIC_AGENT_IP?"ERROR: The environment variable PUBLIC_AGENT_IP is not set."}
+: ${ACCESS_KEY_ID?"ERROR: The environment variable ACCESS_KEY_ID is not set."}
+: ${SECRET_ACCESS_KEY?"ERROR: The environment variable SECRET_ACCESS_KEY is not set."}
+
 # pick the first Kafka broker FQDN (note that -r strips the quotes):
 broker0=`dcos kafka connection | jq -r .dns[0]`
 
 echo deploying the traffic fetcher ...
-# replace the template with the actual value of the broker:
-sed -i '.tmp' "s/BROKER/$broker0/" ./service/traffic-fetcher.json
+if [ "$(uname)" == "Darwin" ]; then
+  # replace the template with the actual value of the broker:
+  sed -i '.tmp' "s/BROKER/$broker0/" ./service/traffic-fetcher.json
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  # replace the template with the actual value of the broker:
+  sed -i'.tmp' "s/BROKER/$broker0/" ./service/traffic-fetcher.json
+fi
 # deploy service:
 dcos marathon app add ./service/traffic-fetcher.json
 # restore template:
@@ -18,8 +27,14 @@ mv ./service/traffic-fetcher.json.tmp ./service/traffic-fetcher.json
 echo ==========================================================================
 
 echo deploying the mapping agent ...
-# replace the template with the actual value of the broker:
-sed -i '.tmp' "s,BROKER,$broker0,; s,_PUBLIC_AGENT_IP,$PUBLIC_AGENT_IP,g; s,_ACCESS_KEY_ID,$ACCESS_KEY_ID,; s,_SECRET_ACCESS_KEY,$SECRET_ACCESS_KEY," ./service/mapping-agent.json
+
+if [ "$(uname)" == "Darwin" ]; then
+  # replace the template with the actual value of the broker:
+  sed -i '.tmp' "s,BROKER,$broker0,; s,_PUBLIC_AGENT_IP,$PUBLIC_AGENT_IP,g; s,_ACCESS_KEY_ID,$ACCESS_KEY_ID,; s,_SECRET_ACCESS_KEY,$SECRET_ACCESS_KEY," ./service/mapping-agent.json
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  # replace the template with the actual value of the broker:
+  sed -i'.tmp' "s,BROKER,$broker0,; s,_PUBLIC_AGENT_IP,$PUBLIC_AGENT_IP,g; s,_ACCESS_KEY_ID,$ACCESS_KEY_ID,; s,_SECRET_ACCESS_KEY,$SECRET_ACCESS_KEY," ./service/mapping-agent.json
+fi
 # deploy service:
 dcos marathon app add ./service/mapping-agent.json
 # restore template:
