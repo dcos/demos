d89e16ad800711b84873f7c72d33543c95a1f8ff
Joerg Schad <joerg@mesosphere.io>
2017-08-04 23:10:53 +0200

    Fixed instructions for new drill version.

--- applogs/1.9/install-drill.sh
+++ applogs/1.9/install-drill.sh
@@ -6,18 +6,16 @@ set -o nounset
 set -o pipefail
 
 : ${PUBLIC_AGENT_IP?"ERROR: The environment variable PUBLIC_AGENT_IP is not set."}
-: ${ACCESS_KEY_ID?"ERROR: The environment variable ACCESS_KEY_ID is not set."}
-: ${SECRET_ACCESS_KEY?"ERROR: The environment variable SECRET_ACCESS_KEY is not set."}
 
 
 echo deploying Apache Drill ...
 
 if [ "$(uname)" == "Darwin" ]; then
   # replace the template with the actual value of the broker:
-  sed -i '.tmp' "s,_PUBLIC_AGENT_IP,$PUBLIC_AGENT_IP,g; s,_ACCESS_KEY_ID,$ACCESS_KEY_ID,; s,_SECRET_ACCESS_KEY,$SECRET_ACCESS_KEY," ./drill/apache-drill.json
+  sed -i '.tmp' "s,_PUBLIC_AGENT_IP,$PUBLIC_AGENT_IP,g;" ./drill/apache-drill.json
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   # replace the template with the actual value of the broker:
-  sed -i'.tmp' "s,_PUBLIC_AGENT_IP,$PUBLIC_AGENT_IP,g; s,_ACCESS_KEY_ID,$ACCESS_KEY_ID,; s,_SECRET_ACCESS_KEY,$SECRET_ACCESS_KEY," ./drill/apache-drill.json
+  sed -i'.tmp' "s,_PUBLIC_AGENT_IP,$PUBLIC_AGENT_IP,g;" ./drill/apache-drill.json
 fi
 # deploy service:
 dcos marathon app add ./drill/apache-drill.json
